f7c18a12552249f1efbcaab254b7dd49b5340ef0
Lautaro Santillan <santillanlautaro03@gmail.com>
2024-06-18 16:27:46 -0300

    making the calculator -v2

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,4 +1,4 @@
-const display = document.querySelector("display");
+const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((item) => {
@@ -7,7 +7,14 @@ buttons.forEach((item) => {
             display.innerText = "";
         } else if(item.id == "backspace"){
             let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.lenght-1);
+            display.innerText = string.substr(0, string.length - 1);
+        } else if(display.innerText != "" && item.id == "equal"){
+            display.innerText = eval(display.innerText);
+        } else if(display.innerText == "" && item.id == "equal"){
+            display.innerText = "Null";
+            setTimeout(() => (display.innerText = ""), 1500);
+        } else {
+            display.innerText += item.id;
         }
     }
 })
