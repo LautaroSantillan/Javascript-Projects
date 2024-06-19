aa6579f42db3a9f44dacd31863302ef8173be663
Lautaro Santillan <santillanlautaro03@gmail.com>
2024-06-19 16:06:34 -0300

    finished the calculator -v3

--- Calculator/script.js
+++ Calculator/script.js
@@ -18,3 +18,15 @@ buttons.forEach((item) => {
         }
     }
 })
+
+// DARK THEME OR LIGHT THEME
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+
+themeToggleBtn.onclick = () => {
+    calculator.classList.toggle("dark");
+    themeToggleBtn.classList.toggle("active");
+    isDark = false;
+}
